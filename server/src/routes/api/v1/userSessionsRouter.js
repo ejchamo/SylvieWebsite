002adcc52567bcd5a13ef1a659b00e8c7a777004
ejchamo <ejchamo@gmail.com>
2024-02-11 20:50:27 -0500

    Add handling for unauthorized user in current session endpoint

--- server/src/routes/api/v1/userSessionsRouter.js
+++ server/src/routes/api/v1/userSessionsRouter.js
@@ -22,6 +22,8 @@ sessionRouter.post("/", (req, res, next) => {
 sessionRouter.get("/current", async (req, res) => {
   if (req.user) {
     res.status(200).json(req.user);
+  } else {
+    res.status(401).json(undefined);
   }
 });
 
