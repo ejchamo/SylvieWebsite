b0e0e63b183f5c748a8c49ccbf12a109580064e5
ejchamo <ejchamo@gmail.com>
2024-02-20 15:40:38 -0500

    Refactor styling and remove unused code

--- client/src/components/paintings/TitlesList.js
+++ client/src/components/paintings/TitlesList.js
@@ -2,14 +2,13 @@ import React from "react";
 import TitleTile from "./TitleTile";
 
 const TitlesList = (props) => {
-  const { titles, setImages, selectedTitle, setSelectedTitle } = props;
+  const { titles, selectedTitle, setSelectedTitle } = props;
 
   const titleTiles = titles.map((title) => {
     return (
       <TitleTile
         key={title}
         title={title}
-        setImages={setImages}
         selectedTitle={selectedTitle}
         setSelectedTitle={setSelectedTitle}
       />
